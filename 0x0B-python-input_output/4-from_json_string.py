1a86b7a82bc41389678ef4c8c781de745ae47504
simon-kachepa <kachepasimon@gmail.com>
2024-02-06 15:35:54 -0800

    Create 4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a function that returns an object represented by a JSON string"""
+import json
+
+
+def from_json_string(my_str):
+    """Returns an object (Python data structure) represented by a JSON string
+
+        Args:
+            my_str (str): JSON string
+
+            Return: an object (Python data structure) represented by a JSON string
+    """
+    return (json.loads(my_str))
